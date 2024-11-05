762dcc61b6bc3069c3059681b900c9b124170cb1
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-06 00:32:38 +0530

    Add files via upload

---
+++ Logical Operators.cpp
@@ -0,0 +1,14 @@
+//Program to print the Logical Operators in C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int a = 4, b = 9;
+    cout << "a && b is " << (a && b) << endl;
+    cout << "a || b is " << (a || b) << endl;
+    cout << "!b is " << (!b) << endl;
+
+    return 0;
+}
